6c548311523ea777374b95692c8b600905025573
Brian Grady <grady.279@osu.edu>
2015-04-11 18:30:11 -0400

    Shows capability to iterate through json datasets
    Gets a json object from a data.state.gov url.  Iterates through the datasets
    and prints the title fields to a text file.  This shows we can store the
    title fields and description text into a data structure, which will be
    passed to a keyword extraction function.

--- python/extract_key_text_from_json_url.py
+++ python/extract_key_text_from_json_url.py
@@ -8,36 +8,40 @@ from pprint import pprint
 
 
 
-URL = "http://data.gov.au/api/3/action/package_show?id=polar-environmental-data-layers"
+URL = {'NY.GOV'     : 'https://data.ny.gov/data.json',
+       'OREGON.GOV' : 'https://data.oregon.gov/data.json',
+       'HAWAII.GOV' : 'https://data.hawaii.gov/data.json'}
 
 def main():
-    """Main entry point for the script."""
-    #get_raw_text(URL)
-    get_json(URL)
+    '''.'''
+    
+    
+    
+    json_object = get_json_from_url( URL['NY.GOV'] )
+    
+    assert(json_object['conformsTo'] == 'https://project-open-data.cio.gov/v1.1/schema')
+    
+    with open('titles.txt','w') as output:
+        for dataset in json_object['dataset']:
+            #output.write(dataset['title'] + '\n')
+            
+    
 
-    
-    
-    
-    
-    
-    
+def get_json_from_url(url):
+    '''.'''
+    response = urllib.request.urlopen(url)
+    reader = codecs.getreader('utf-8')
+    json_object = json.load(reader(response))
+    return json_object
+  
+  
 def get_text_from_url(url):
     '''.'''
     response = urllib.request.urlopen(url)
     html = response.read()
     with open('text.txt','w') as output:
         html = str(html)
-        output.write(html)
-    
-def get_json(url):
-    '''.'''
-    response = urllib.request.urlopen(url)
-    reader = codecs.getreader("utf-8")
-    data = json.load(reader(response))
-    
-    pprint(data["result"]["notes"])
-    
-    
-    
+        output.write(html)    
+       
 if __name__ == '__main__':
     sys.exit(main())
