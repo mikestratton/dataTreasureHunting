13beb10568f324cb60ce5a9c73fb2c732faaed2a
Brian Grady <grady.279@osu.edu>
2015-04-11 21:51:19 -0400

    Combines essential fields into single string
    The fields keyword, theme, title, and description are combined into a
    single string for each dataset.
    
    Cannot figure out how to avoid UnicodeEncodeError when writing to
    a text file, the viewing of which can be helpful to understand how the
    data is being manipulated.  The try except block
    is the solution for now.
    
    Sloppy but okay for now...

--- python/extract_key_text_from_json_url.py
+++ python/extract_key_text_from_json_url.py
@@ -6,6 +6,7 @@ import codecs
 
 from pprint import pprint
 
+from collections import namedtuple
 
 
 URL = {'NY.GOV'     : 'https://data.ny.gov/data.json',
@@ -14,16 +15,37 @@ URL = {'NY.GOV'     : 'https://data.ny.gov/data.json',
 
 def main():
     '''.'''
-    
-    
-    
+        
     json_object = get_json_from_url( URL['NY.GOV'] )
     
-    assert(json_object['conformsTo'] == 'https://project-open-data.cio.gov/v1.1/schema')
+    assert(json_object['conformsTo'] == 'https://project-open-data.cio.gov/v1.1/schema')    
     
-    with open('titles.txt','w') as output:
-        for dataset in json_object['dataset']:
-            #output.write(dataset['title'] + '\n')
+    with open('visual_of_combined.txt','w') as output:
+        for idx,dataset in enumerate(json_object['dataset']):
+            if 'keyword' in dataset.keys():
+                keywords = ' '.join(str(x) for x in dataset['keyword'])
+            if 'theme' in dataset.keys():
+                theme = ' '.join(str(x) for x in dataset['theme'])
+            if 'title' in dataset.keys():
+                title = str(dataset['title'])
+            if 'description' in dataset.keys():
+                description = str(dataset['description'])
+                
+            dataset_essentials = (keywords,theme,title,description)
+            combined = ' '.join(dataset_essentials)                    #<<<<<<<<<<<<<<<<<<<< combined is a string containing potential keywords
+
+            ############################################################################
+            #     Insert code/function, or call function, to get word count here
+            
+            ############################################################################
+            #combined.encode('utf-8', errors = 'ignore')
+            
+            output.write(str(idx) + '\n')
+            try:
+                output.write(combined + '\n')
+            except UnicodeEncodeError:
+                pass
+            output.write('***************************\n')
             
     
 
@@ -32,6 +54,8 @@ def get_json_from_url(url):
     response = urllib.request.urlopen(url)
     reader = codecs.getreader('utf-8')
     json_object = json.load(reader(response))
+    #str_response = response.readall().decode('utf-8')
+    #json_object = json.loads(str_response)
     return json_object
   
   
